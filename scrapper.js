037c0bf9c8ac19f582f3da89639e09b9d0f21a34
Isaies Andrés Nebot <isaies@MacBook-Pro-de-Isaies.local>
2016-05-26 18:50:52 +0200

    scrapper

--- scrapper.js
+++ scrapper.js
@@ -1,59 +1,72 @@
-var express = require('express');
 var request = require('request');
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
+
+/*
+// Models for mongo
 var mongoose = require('mongoose');
 
-// Models
-/*
 var Degree = mongoose.model("Degree");
 var Subject = mongoose.model("Subject");
-*/
+ */
+
 // Constants
 const COURSEYEAR = 2015;
 const BASEURL = 'https://e-ujier.uji.es/pls/www/';
 const DREGREELISTURL = "!gri_ass.lleu_portada_g?p_curso_init=";
 
 
+// Global JSON ariable carreras
+var carreras ={};
 
-// Scraps and stores first lleu's view: Subjects
+
+// Scraps and stores first lleu's view: Degrees.
 request(BASEURL+DREGREELISTURL+COURSEYEAR, {encoding:'utf8'}, function (err, response, body) {
-  if (!err && response.statusCode == 200) {
 
-    $ = cheerio.load(body);
+		if (!err && response.statusCode == 200) {
 
-    var asignaturas={};
+		//Loads the whole html to be parsed
+		$ = cheerio.load(body);
 
-    $('li a', '.listaTitulaciones' ).each(function(i, elem) {
-        // new JSON (id, name, url)
-        asignaturas[$(this)[0].children[0].data]=$(this)[0].attribs.href;
-        });
+		//Selects all degrees
+		$('li a', '.listaTitulaciones' ).each(function(i, elem) {
 
-    console.log(asignaturas);
+			// Fills the JSON=> id: [name, url]
+			var url=$(this)[0].attribs.href;
+			var id=url.slice(-3);
+			carreras[id]= [$(this)[0].children[0].data, url];
+			});
 
-  }else {
-    console.log(err);
+		
+		console.log("Start scrapping degrees");
+		
 
-  }
+
+///// -------- Falta comunicar cuando termina de guardar el json al siguiente for
+
+		// Start scraping urls
+		for(i=0; i<carreras.length; i++){
+			
+			// Degree url generation
+			var degreeUrl=carreras[i][1];
+			console.log("Carrera: "+i+". URL:"+ degreeUrl);
+			
+			// Scraps and stores all subjects
+			request(BASEURL+degreeUrl, {encoding:'utf8'}, function (err, response, body) {
+					
+					if (!err && response.statusCode == 200) {
+						
+						// Loads degree info
+						$ = cheerio.load(body);
+
+					}else {console.log("Error 2o request, asignaturas:", err);}
+			});
+
+		}
+
+		}else {console.log("Error 1er request, carreras", err);}
+
 });
 
 
-// Scraps and stores all subj
-request(BASEURL+COURSEYEAR, {encoding:'utf8'}, function (err, response, body) {
-  if (!err && response.statusCode == 200) {
 
-    $ = cheerio.load(body);
-
-    var asignaturas={};
-
-    $('li a', '.listaTitulaciones' ).each(function(i, elem) {
-        asignaturas[$(this)[0].children[0].data]=$(this)[0].attribs.href;
-        });
-
-    console.log(asignaturas);
-
-  }else {
-    console.log(err);
-
-  }
-});
